dcde431f861bd4cee7848c56a3da670d4481e43e
Dennis Nedkov <Dnedkov9274@jh-mba158-12.d214.org>
2018-10-05 11:14:11 -0500

    Initial Commit

---
+++ Comission/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Comission
+//
+//  Created by Dennis Jivko Nedkov on 10/5/18.
+//  Copyright © 2018 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
